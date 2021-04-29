b00d31d1fb8c5266c5a9ff590208074131f4011f
JeViCo <jevico-official@outlook.com>
2021-04-29 23:26:41 +0300

    Добавлен блок анимаций, движение изображений и текста

--- test/ui_main.lua
+++ test/ui_main.lua
@@ -1,27 +1,55 @@
 local scx, scy = guiGetScreenSize( )
+local tocolor = tocolor
 local menuData = { }
 local menuState
 
-local menuConfig = {
-	section = {
-		count = 5, -- Количество секций
-		offset = 200, -- Расстояние от центра экрана до центра секции (adapt)
-		cRadius = 150, -- Радиус круга (adapt)
-		imgRadius = 68, -- Размер иконки (adapt)
+local menuConfig =
+{
+	count = 5, -- Количество секций
+	offset = 200, -- Расстояние от центра экрана до центра секции (adapt)
+	cRadius = 150, -- Радиус круга (adapt)
+	imgRadius = 68, -- Размер иконки (adapt)
+	contentOffset = 10, -- adapt
+	--borderOffset = 30, -- adapt
+	animTime = 500,
+	animEasing = 'InOutQuad',
+	content =
+	{
+		{
+			title = 'Measure'
+		},
+		{
+			title = 'Tools'
+		},
+		{
+			title = 'Scissors'
+		},
+		{
+			title = 'Brush'
+		},
+		{
+			title = 'Pencil'
+		},
 	}
 }
-menuConfig.rotAmount = 360 / menuConfig.section.count
+menuConfig.rotAmount = 360 / menuConfig.count
 
 function menuManager( state )
 	if menuState and state or not menuState and not state then return end
 	if state then
 		menuData.sections = { }
-		for i = 1, menuConfig.section.count do
+		for i = 1, menuConfig.count do
 			local rot = menuConfig.rotAmount*i - menuConfig.rotAmount/2
-			local posX, posY = findPosition( scx/2, scy/2, rot, menuConfig.section.offset )
-			menuData.sections[ i ] = {
-				x = posX, y = posY, -- no adapt
-				path = 'assets/images/icon_' .. i .. '.png'
+			local posX, posY = findPosition( scx/2, scy/2, rot, menuConfig.offset )
+
+			menuData.sections[ i ] =
+			{
+				x = posX, y = posY,
+				path = 'assets/images/icon_' .. i .. '.png',
+				transition = dxCreateTransition(
+					false, 0, menuConfig.contentOffset,
+					menuConfig.animTime, menuConfig.animEasing
+				)
 			}
 		end
 
@@ -38,23 +66,53 @@ end
 
 function renderMenu( )
 	for i = 1, #menuData.sections do
+		local sect = menuData.sections[ i ]
+		local cOffset = dxGetTransitionValue( menuData.sections[ i ].transition )
+		local scale = cOffset/menuConfig.contentOffset
+
 		dxDrawImage(
-			menuData.sections[ i ].x - menuConfig.section.cRadius * 0.5,
-			menuData.sections[ i ].y - menuConfig.section.cRadius * 0.5,
-			menuConfig.section.cRadius, menuConfig.section.cRadius,
+			sect.x - menuConfig.cRadius/2,
+			sect.y - menuConfig.cRadius/2,
+			menuConfig.cRadius,
+			menuConfig.cRadius,
 			'assets/images/circle.png'
 		)
 		dxDrawImage(
-			menuData.sections[ i ].x - menuConfig.section.imgRadius/2,
-			menuData.sections[ i ].y - menuConfig.section.imgRadius/2,
-			menuConfig.section.imgRadius, menuConfig.section.imgRadius,
-			menuData.sections[ i ].path
+			sect.x - menuConfig.imgRadius/2,
+			sect.y - menuConfig.imgRadius/2 - cOffset,
+			menuConfig.imgRadius,
+			menuConfig.imgRadius,
+			sect.path
 		)
+
+		dxDrawText(
+			menuConfig.content[ i ].title,
+			sect.x - menuConfig.imgRadius / 1.75,
+			sect.y + menuConfig.imgRadius / 2 + cOffset,
+			sect.x + menuConfig.imgRadius / 1.75,
+			sect.y + menuConfig.imgRadius / 2 + cOffset,
+			tocolor( 0, 0, 0, scale * 255 ), scale * 0.5 + 1, -- adapt
+			'default', 'center', 'center'
+		)
+
+		local inPos = inPosition(
+			sect.x,
+			sect.y,
+			menuConfig.cRadius
+		)
+		
+		if sect.selected and not inPos then
+			dxSetTransitionState( menuData.sections[ i ].transition, false )
+			sect.selected = nil
+		elseif not sect.selected and inPos then
+			menuData.sections[ i ].selected = true
+			dxSetTransitionState( menuData.sections[ i ].transition, true )
+		end
 	end
 end
 
 addCommandHandler('testMenu', function( )
-	menuManager( not menuState )
+	menuManager( not menuState ) -- Добавить анимацию при появлении Timer + 
 end)
 
-menuManager(true)
+menuManager( true )
--- test/utils.lua
+++ test/utils.lua
@@ -14,9 +14,78 @@ function findPosition( x, y, rot, offset )
 		y + math.cos ( math.rad ( rot ) ) * ( offset or 1 )
 end
 
-function inPosition ( x, y, width, height )
+function inPosition ( x, y, radius )
 	local sx, sy = guiGetScreenSize ( )
 	local cx, cy = getCursorPosition ( )
 	cx, cy = ( cx * sx ), ( cy * sy )
-	return ( ( cx >= x and cx <= x + width ) and ( cy >= y and cy <= y + height ) )
+	return (cx - x)^2 + (cy - y)^2 < (radius/2)^2
 end
+
+local transitions = { }
+local min = math.min
+
+function dxCreateTransition( bool, from, to, time, easing )
+	local ID = 1
+	while transitions[ ID ] do
+		ID = ID + 1
+	end
+
+	transitions[ ID ] = {
+		state = bool,
+		time = time or 1000,
+		tick = getTickCount( ),
+		easing = easing or 'Linear',
+		startPoint = from, endPoint = to,
+		timer = Timer( triggerEvent, time, 1, 'onDxTransitionOver', resourceRoot, ID )
+	}
+
+	if bool then
+		transitions[ ID ].active = true
+		transitions[ ID ].timer = Timer ( onDxTransitionOver, time, 1, ID )
+		transitions[ ID ].from, transitions[ ID ].to = from, to
+	else
+		transitions[ ID ].tick = transitions[ ID ].tick - transitions[ ID ].time
+		transitions[ ID ].from, transitions[ ID ].to = to, from
+	end
+
+	return ID
+end
+
+function dxRemoveTransition( ID )
+	if not transitions[ ID ] then return false end
+	if transitions[ ID ].timer and transitions[ ID ].valid then
+		transitions[ ID ].timer:destroy( )
+	end
+	clearTable( transitions[ ID ] )
+	transitions[ ID ] = nil
+end
+
+function dxGetTransitionValue ( ID )
+	if not transitions[ ID ] then return false end
+	local progress = ( getTickCount ( ) - transitions[ ID ].tick ) / transitions[ ID ].time
+
+	return transitions[ ID ].from + ( transitions[ ID ].to - transitions[ ID ].from )
+		* getEasingValue ( min ( progress, 1 ), transitions[ ID ].easing )
+end
+
+function dxSetTransitionState ( ID, bool )
+	if not transitions[ ID ] then return false end
+	if transitions[ ID ].state == bool then return true end
+
+	transitions[ ID ].state = bool
+	transitions[ ID ].from = dxGetTransitionValue ( ID )
+	transitions[ ID ].to = bool and transitions[ ID ].endPoint or transitions[ ID ].startPoint
+	transitions[ ID ].tick = getTickCount ( )
+
+	local timeleft = transitions[ ID ].time
+
+	if transitions[ ID ].timer and transitions[ ID ].timer.valid then
+		timeleft = timeleft - transitions[ ID ].timer:getDetails( )
+		transitions[ ID ].timer:destroy ( )
+	end
+	transitions[ ID ].active = true
+	transitions[ ID ].timer = Timer( triggerEvent, timeleft, 1, 'onDxTransitionOver', resourceRoot, ID )
+	return true
+end
+
+addEvent( 'onDxTransitionOver', false )
